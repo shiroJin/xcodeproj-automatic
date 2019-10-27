0d8ba0abec817ee86beb8187a2368ec3369df1c5
jqb <946502866@qq.com>
2019-10-27 22:25:04 +0800

    pull, merge, commit action

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -2,37 +2,37 @@ require_relative '../script/app'
 require_relative '../script/XcodeProject'
 require 'git'
 class ProjectController < ApplicationController
+  @@project_path = '/Users/remain/Desktop/script-work/ButlerForFusion'
   def add_new_project
     company_code = params["kCompanyCode"]
     branch_name = "proj-#{company_code}-snapshot"
-    project_path = '/Users/remain/Desktop/script-work/ButlerForFusion'
+    project_path = @@project_path
     tag_name = params["tag"]
 
-    git = Git.open(project_path)
-    git.branches.each do |branch|
+    git = Git.open(@@project_path)
+    if branch = git.branches.find { |b| b.name == branch_name }
       raise "branch #{branch_name} alread existed"
     end
     
-    git.tags.each do |t|
-      tag = t if t.name == tag_name
+    if tag = git.tags.find { |t| t.name == tag_name }
+      raise "tag #{tag_name} is not existed" unless tag
     end
-    raise "tag #{tag_name} is not existed" unless tag
 
-    XcodeProject.
+    cmd = "git --git-dir=#{@@project_path}/.git -b #{branch_name} #{tag_name}"
+    exec cmd
+    raise "checkout new branch failed" if git.current_branch.name != branch_name
 
     XcodeProject.new_target(project_path, params.as_json)
     render()
   end
 
   def edit_project
-    project_path = '/Users/remain/Desktop/script-work/ButlerForFusion'
-    XcodeProject.edit_project(project_path, params['companyCode'], params['updateInfo'].as_json)
+    XcodeProject.edit_project(@@project_path, params['companyCode'], params['updateInfo'].as_json)
     render()
   end
 
   def fetch_project_info
-    project_path = '/Users/remain/Desktop/script-work/ButlerForFusion'
-    data = XcodeProject.fetch_target_info(project_path, params["companyCode"])
+    data = XcodeProject.fetch_target_info(@@project_path, params["companyCode"])
     render :json => data
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,12 @@ Rails.application.routes.draw do
   get 'project/projectInfo', to: 'project#fetch_project_info'
   post 'project/addProject', to: 'project#add_new_project'
   post 'project/editProject', to: 'project#edit_project'
+
+  #workspace
+  post 'workspace/pull', to: 'workspace#pull'
+  get 'workspace/tags', to: 'workspace#tags'
+  post 'workspace/clean', to: 'workspace#stash'
+  post 'workspace/commit', to: 'workspace#commit'
+  post 'workspace/merge', to: 'workspace#merge'
+
 end
