55828ec96dd950ba6320ec8bebf6a1d6b746a650
quanbin.jin <quanbin.jin@uama.com.cn>
2019-12-06 20:25:58 +0800

    示例变量问题临时处理

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -7,14 +7,15 @@ class ProjectController < ApplicationController
   @project_path = '/Users/panjiafei/freeMyMac/workspace/ButlerForFusion'
   
   def git
-    @git ||= Git.open(@project_path)
+    puts @@project_path
+    @git ||= Git.open(@@project_path)
   end
 
   # 新增分支及target
   def add_new_project
     company_code = params["kCompanyCode"]
     branch_name = "proj-#{company_code}-snapshot"
-    project_path = @project_path
+    project_path = @@project_path
     tag_name = params["tag"]
 
     if branch = self.git.branches.find { |b| b.name == branch_name }
@@ -25,7 +26,7 @@ class ProjectController < ApplicationController
       raise "tag #{tag_name} is not existed" unless tag
     end
 
-    cmd = "git --git-dir=#{@project_path}/.git -b #{branch_name} #{tag_name}"
+    cmd = "git --git-dir=#{@@project_path}/.git -b #{branch_name} #{tag_name}"
     exec cmd
     raise "checkout new branch failed" if self.git.current_branch.name != branch_name
 
@@ -35,13 +36,13 @@ class ProjectController < ApplicationController
 
   # 编辑项目
   def edit_project
-    XcodeProject.edit_project(@project_path, params['companyCode'], params['updateInfo'].as_json)
+    XcodeProject.edit_project(@@project_path, params['companyCode'], params['updateInfo'].as_json)
     render()
   end
 
   # 获取项目信息
   def fetch_project_info
-    data = XcodeProject.fetch_target_info(@project_path, params["companyCode"])
+    data = XcodeProject.fetch_target_info(@@project_path, params["companyCode"])
     render :json => data
   end
 
@@ -60,7 +61,7 @@ class ProjectController < ApplicationController
   # 获取当前项目
   def fetch_current_project
     app = App.find_app_with_branch(self.git.current_branch)
-    data = XcodeProject.fetch_target_info(@project_path, app.company_code)
+    data = XcodeProject.fetch_target_info(@@project_path, app.company_code)
     render :json => data
   end
 
@@ -90,14 +91,14 @@ class ProjectController < ApplicationController
 
   #下拉代码
   def pull
-    cmd = "git --git-dir=#{@project_path}/.git pull 2>&1"
+    cmd = "git --git-dir=#{@@project_path}/.git pull 2>&1"
     IO.popen(cmd) { |result|
       render :json => { 'msg' => result.read }
     }
   end
 
   def worktree_is_dirty
-    cmd = "git --git-dir=#{@project_path}/.git --work-tree=#{@project_path} status"
+    cmd = "git --git-dir=#{@@project_path}/.git --work-tree=#{@@project_path} status"
     IO.popen(cmd) { |result|
       message = result.read
       dirty = false
@@ -109,7 +110,7 @@ class ProjectController < ApplicationController
   end
 
   def is_dirty
-    cmd = "git --git-dir=#{@project_path}/.git --work-tree=#{@project_path} status"
+    cmd = "git --git-dir=#{@@project_path}/.git --work-tree=#{@@project_path} status"
     IO.popen(cmd) { |result|
       message = result.read
       dirty = false
