ad1b49be5e59be3ba68e02491fd7b9ecd95c8486
quanbin.jin <quanbin.jin@uama.com.cn>
2020-02-18 16:23:29 +0800

    package script update“

--- app/controllers/package_controller.rb
+++ app/controllers/package_controller.rb
@@ -1,5 +1,4 @@
 require "plist"
-require "pty"
 
 class PackageController < ApplicationController
 
@@ -20,12 +19,14 @@ class PackageController < ApplicationController
       archive_path = File.join(archive_dir, "#{target}.xcarchive")
       export_path = File.join(archive_dir, target)
 
-      unlock_login_keychain
       pod_install
+
       archive(target, archive_path)
       raise "archive failed" unless File.exist? archive_path
+
       generate_export_plist(configuration, method, export_plist_path)
       raise "generate export plist failed" unless File.exist? export_plist_path
+
       export_archive(archive_path, export_plist_path, export_path)
       raise "export archive failed" unless export_path
 
@@ -41,6 +42,54 @@ class PackageController < ApplicationController
     cmd = "security unlock-keychain -p Uama123 ~/Library/Keychains/login.keychain"
     IO.popen(cmd) { |std|
       puts std.read
+    }
+  end
+
+  def generate_export_plist(configuration, method, export_plist_path)
+    sign = configuration["CODE_SIGN_IDENTITY"]
+    provision = configuration["PROVISIONING_PROFILE_SPECIFIER"]
+    team = configuration["DEVELOPMENT_TEAM"]
+    bundle_id = configuration["PRODUCT_BUNDLE_IDENTIFIER"]
+
+    export_hash = Hash.new
+    export_hash["destination"] = "export"
+    export_hash["signingStyle"] = "manual"
+    export_hash["compileBitcode"] = false
+    export_hash["method"] = method
+    export_hash["teamID"] = team
+    export_hash["signingCertificate"] = sign
+    export_hash["provisioningProfiles"] = { bundle_id => provision }
+    export_hash["thinning"] = "<none>"
+
+    IO.write(export_plist_path, export_hash.to_plist)
+  end
+
+  def pod_install
+    rvm_path = ENV["rvm_path"]
+    env = {
+      'GEM_HOME' => "#{rvm_path}/gems/ruby-2.6.3",
+      'GEM_PATH' => "#{rvm_path}/gems/ruby-2.6.3",
+      'BUNDLE_BIN_PATH' => '',
+      'BUNDLE_GEMFILE' => '',
+    }
+    Open3.popen3(env, 'pod install') { |stdin, stdout, stderr, wait_thr|
+      stdout.each { |line| print line }
+    }
+  end
+
+  def archive(scheme, archive_path, configuration="Release")
+    workspace = Dir.entries(Dir.pwd).find { |e| e.index('workspace') }
+    cmd = "xcodebuild clean archive -workspace #{workspace} -scheme #{scheme} -configuration #{configuration} -archivePath #{archive_path}"
+    Open3.popen3(cmd) { |i, o, e, t|
+      puts o.each{ |line| print line }
+    }
+    return archive_path
+  end
+
+  def export_archive(archive_path, export_plist_path, export_path)
+    cmd = "xcodebuild -exportArchive -archivePath #{archive_path} -exportPath #{export_path} -exportOptionsPlist #{export_plist_path}"
+    Open3.popen3(cmd) { |i, o, e, t|
+      puts o.each{ |line| print line }
     }
   end
 
@@ -60,69 +109,6 @@ class PackageController < ApplicationController
       Dir.mkdir(build_dir)
     end
     return build_dir
-  end
-
-  def generate_export_plist(configuration, method, export_plist_path)
-    sign = configuration["CODE_SIGN_IDENTITY"]
-    provision = configuration["PROVISIONING_PROFILE_SPECIFIER"]
-    team = configuration["DEVELOPMENT_TEAM"]
-    certificate = configuration["CODE_SIGN_IDENTITY"]
-    bundle_id = configuration["PRODUCT_BUNDLE_IDENTIFIER"]
-
-    export_hash = Hash.new
-    export_hash["signingStyle"] = "manual"
-    export_hash["compileBitcode"] = false
-    export_hash["method"] = method
-    export_hash["teamID"] = team
-    export_hash["signingCertificate"] = certificate
-    export_hash["provisioningProfiles"] = { bundle_id => provision }
-
-    IO.write(export_plist_path, export_hash.to_plist)
-  end
-
-  def pod_install
-    rvm_path = ENV["rvm_path"]
-    env = {
-      'GEM_HOME' => "#{rvm_path}/gems/ruby-2.6.3",
-      'GEM_PATH' => "#{rvm_path}/gems/ruby-2.6.3",
-      'BUNDLE_BIN_PATH' => '',
-      'BUNDLE_GEMFILE' => '',
-    }
-    IO.popen(env, "pod install") { |result|
-      result.each { |line| print line }
-    }
-  end
-
-  def archive(scheme, archive_path, configuration="Release")
-    workspace = Dir.entries(Dir.pwd).find { |e| e.index('workspace') }
-    cmd = "xcodebuild clean archive -workspace #{workspace} -scheme #{scheme} -configuration #{configuration} -archivePath #{archive_path}"
-    begin
-      PTY.spawn(cmd) do |stdout, stdin, pid|
-        begin
-          stdout.each { |line| print line }
-        rescue Errno::EIO
-          puts "Errno:EIO error, but this probably just means " + "that the process has finished giving output"
-        end
-      end
-    rescue PTY::ChildExited
-      puts "The child process exited!"
-    end
-    return archive_path
-  end
-
-  def export_archive(archive_path, export_plist_path, export_path)
-    cmd = "xcodebuild -exportArchive -archivePath #{archive_path} -exportPath #{export_path} -exportOptionsPlist #{export_plist_path}"
-    begin
-      PTY.spawn(cmd) do |stdout, stdin, pid|
-        begin
-          stdout.each { |line| print line }
-        rescue Errno::EIO
-          puts "Errno:EIO error, but this probably just means " + "that the process has finished giving output"
-        end
-      end
-    rescue PTY::ChildExited
-      puts "The child process exited!"
-    end
   end
 
 end
